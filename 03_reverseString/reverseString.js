e5f569e4164f472e26e267cb59b99a04e9ed07e6
Logodum <15616120+PavelProvotorov@users.noreply.github.com>
2023-06-27 07:25:07 +0300

    Task 03 Solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text = "") {
+    let reverse_text = ""
+    for (let char = text.length-1; char >= 0; char--){
+        reverse_text += text[char]
+    };
+    return reverse_text;
 };
 
 // Do not edit below this line
