0da755cb69e854e1a00c59648c160d441b5e09c6
Logodum <15616120+PavelProvotorov@users.noreply.github.com>
2023-08-16 21:35:08 +0300

    Task 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(data) {
+    const person = data.reduce((person_a, person_b) => {
+        const current_year = new Date().getFullYear();
 
+        const person_a_age = person_a.yearOfDeath
+            ? person_a.yearOfDeath - person_a.yearOfBirth
+            : current_year - person_a.yearOfBirth;
+
+        const person_b_age = person_b.yearOfDeath 
+            ? person_b.yearOfDeath - person_b.yearOfBirth
+            : current_year - person_b.yearOfBirth;
+
+        return person_a_age < person_b_age ? person_b: person_a;
+        });
+    return person
 };
 
 // Do not edit below this line
