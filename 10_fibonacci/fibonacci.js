37e5d386c2931c59fbe499f37c3cb99bd591cc77
Logodum <15616120+PavelProvotorov@users.noreply.github.com>
2023-08-16 20:28:01 +0300

    Task 10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(idx) {
+    idx = parseInt(idx)
+    if (Math.sign(idx) === -1){
+        return "OOPS"
+    }
 
+    let num_a = 0
+    let num_b = 1
+    let num_c = idx
+
+    for(let i = 1; i < idx; i++){
+        num_c = num_a + num_b
+        num_a = num_b
+        num_b = num_c
+    }
+    return num_c
 };
 
 // Do not edit below this line
