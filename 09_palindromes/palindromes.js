985104b7297d7dad8333af187ba9e82728d40132
Logodum <15616120+PavelProvotorov@users.noreply.github.com>
2023-08-16 20:00:10 +0300

    Task 09 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let string_original = string.toLowerCase().replace(/[^a-z0-9]/g, "")
+    let string_reverse = string_original.split("").reverse().join("")
+    return string_original == string_reverse
 };
 
 // Do not edit below this line
