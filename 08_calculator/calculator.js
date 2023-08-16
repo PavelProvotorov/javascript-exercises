958c18a2d2d77744a444caff62fac6c3e2ceafa7
Logodum <15616120+PavelProvotorov@users.noreply.github.com>
2023-08-16 19:31:17 +0300

    Task 08 solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    return arr.reduce((total, value) => total + value, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(...arr) {
+    return arr.reduce((total, value) => total * value)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num === 0 || num === 1){
+        return 1
+    };
+    let result = 1
+    for(let i = 2; i <= num; i++){
+        result *= i
+    };
+    return result
 };
 
 // Do not edit below this line
